1129e25c921ec852ebb9d9f7f31712b392aa2a0c
Mai Vang <vmaineng@gmail.com>
2024-12-25 17:41:17 -0800

    added in choices

--- eat_game.py
+++ eat_game.py
@@ -1,5 +1,5 @@
 def play():
-    user = input("'s' for sushi, 't' for thai food, or 'd' for desserts")
+    user = input("What do you want to eat today? 's' for sushi, 't' for thai food, or 'd' for desserts\n")
     computer = random.choice(['s', 't', 'd'])
 
     if user == computer:
@@ -8,9 +8,10 @@ def play():
     if chose_eat(user, computer):
         return 'You get to eat'
     
-    return 'Let\'s play again
+    return 'Let\'s play again'
 
     def chose_eat(eater, computer):
+        #return true if eater makes a selection
         if (eater == 't' and computer == 's') or (eater == 'd' and computer =='s') or (eater == 't' and computer == 'd'):
             return True
 
