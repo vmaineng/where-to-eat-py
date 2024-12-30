5f2bfd02aa4e0569d002c6366c9122e5f95c3387
Mai Vang <vmaineng@gmail.com>
2024-12-30 09:25:03 -0800

    added in while loop

--- eat_game.py
+++ eat_game.py
@@ -1,18 +1,22 @@
+import random
+
+options = ['sushi', 'thai', 'italian']
+
 def play():
-    user = input("What do you want to eat today? 's' for sushi, 't' for thai food, or 'd' for desserts\n")
-    computer = random.choice(['s', 't', 'd'])
-
-    if user == computer:
-        return 'please make a choice'
-
-    if chose_eat(user, computer):
-        return 'You get to eat'
+    user_choice = input(f"What do you want to eat today? Chose from {', '.join(options)}\n")
+  
+    while user_choice not in options:
+        return f"{user_choice} is not in the list of the options. Please pick again"
+        continue
     
-    return 'Let\'s play again'
+        computer_choice = random.choice(options)
+    
+        if user_choice == computer_choice:
+            return "Let's eat"
+            break
+        else:
+            return f"You chose {user_choice}, but the computer suggested {computer_choice}"
+            break
 
-    def chose_eat(eater, computer):
-        #return true if eater makes a selection
-        if (eater == 't' and computer == 's') or (eater == 'd' and computer =='s') or (eater == 't' and computer == 'd'):
-            return True
+print(play())
 
-    print(play)
